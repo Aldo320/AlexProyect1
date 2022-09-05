17a7760917bd7e2986ea28e745f827c8e3786087
Aldo320 <aldoantunez12@gmail.com>
2022-09-05 17:07:28 -0300

    AlexProyect

---
+++ app.js
@@ -0,0 +1,72 @@
+function saludar (Nombre= Apellido ){
+    console.log (Hola ${Nombre} " " ${Apellido} " ")
+}
+
+let option = prompt (`Elja Tu Marca de Calzado Favorita:
+1:Nike
+2:Adidas
+3:Puma
+4:Reebok
+`)
+
+
+
+const nombre = "Nike AirMax"
+const precio = 5490
+const disponible = true
+
+const producto = {
+    nombre: "Nike Airmax",
+    precio: 5490,
+    disponible: true,
+}
+
+console.log(producto)
+
+const nombre1 = "Adidas Coreracer"
+const precio1 = 4290
+const disponible1 = true
+
+const producto1 = {
+    nombre: "Adidas Coreracer",
+    precio: 4290,
+    disponible: true,
+}
+
+console.log(producto1)
+
+const nombre2 = "Puma Xray Deportivo"
+const precio2 = 4990
+const disponible2 = true
+
+const producto2 = {
+    nombre: "Puma Xray Deportivo",
+    precio: 4990,
+    disponible: true,
+}
+
+console.log(producto2)
+
+const nombre3 = "Reebok Triplehall"
+const precio3 = 2850
+const disponible3 = true
+
+const producto3 = {
+    nombre: "Reebok Triplehall",
+    precio: 2850,
+    disponible: true,
+}
+
+console.log(producto3)
+
+const nombre4 = "New Balance Lifestyle"
+const precio4 = 3990
+const disponible4 = true
+
+const producto4 = {
+    nombre: "New Balance Lifestyle",
+    precio: 3990,
+    disponible: true,
+}
+
+console.log(producto4)
